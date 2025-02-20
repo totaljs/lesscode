9ce526bec3b4b2a033a031710ec31d25fbcb4644
petersirka <petersirka@gmail.com>
2025-02-20 08:39:40 +0100

    Added bundle.

---
+++ bundle.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+require('total5');
+
+var path = '--bundles--';
+
+function buildplugin(name, callback) {
+	console.log('| |--', name + '.bundle');
+	Total.backup(path + '/' + name + '.bundle', PATH.root(), callback, function(path, isdir) {
+		return path === '/' || path === '/plugins/' || (path.indexOf('plugins/' + name) !== -1);
+	});
+}
+
+console.log('|-- Total.js bundle compiler');
+console.time('|-- Compilation');
+
+console.log('| |--', 'app.bundle');
+Total.backup(path + '/app.bundle', PATH.root(), function() {
+	F.Fs.readdir(PATH.root('plugins'), function(err, response) {
+
+		if (err) {
+			console.timeEnd('|-- Compilation');
+			return;
+		}
+
+		response.wait(function(key, next) {
+			buildplugin(key, next);
+		}, function() {
+			console.timeEnd('|-- Compilation');
+		});
+	});
+}, function(path, isdir) {
+
+	if (!isdir)
+		return path.split('/').length > 2;
+
+	var p = path.split('/').trim();
+
+	if (!p[0] || (p.length === 1 && p[0] === 'plugins'))
+		return true;
+
+	var allowed = ['databases', 'definitions'];
+
+	for (var m of allowed) {
+		if (path.indexOf(m) === 1)
+			return true;
+	}
+
+	return false;
+});
